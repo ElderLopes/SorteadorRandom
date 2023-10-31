19b77da16aceafadff1360c63fdbb7f50dc40032
ElderLopes <elderfl85@hotmail.com>
2023-10-31 15:13:04 -0300

    Responsividade update

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
-function numberGenerator(){
+function numberGenerator() {
 
     const number1 = Math.ceil(document.querySelector('.number1').value)
     const number2 = Math.floor(document.querySelector('.number2').value)
 
-   const result = Math.floor(Math.random() * (number2 - number1 + 1)) + number1;
 
-   alert(result)
- 
+    if (number1 >= number2) {
+
+        alert('O 1º número tem que ser menor que o 2º número')
+    }
+    else {
+        const result = Math.floor(Math.random() * (number2 - number1 + 1)) + number1;
+        alert(`O numero sorteado foi ${result}`)
+    }
+
+
 }
